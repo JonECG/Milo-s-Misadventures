20291b93f44d03b069d15b00baa2049dfcaad26c
Talan Lousignont <Talan.L.Lousignont@gmail.com>
2014-09-30 17:50:10 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/murderrunner@4 c5c08a46-e941-3541-a803-61b983f3127b

---
+++ MurderRunner/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 startPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveTowardsPlayer ();
+	}
+
+	void moveTowardsPlayer(){
+		Vector3 moveDirection = new Vector3(this.transform.position.x - player.transform.position.x, this.transform.position.y - player.transform.position.y, this.transform.position.z - player.transform.position.z);
+		this.transform.position = this.transform.position - (moveDirection.normalized * (Time.deltaTime*2));
+	}
+}
