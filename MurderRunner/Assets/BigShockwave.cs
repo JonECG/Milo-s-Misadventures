580b10a2ece67676c5fdfdd932be53dcb3260dd8
Colter Cvach <ccvach@student.neumont.edu>
2014-10-02 16:55:36 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/murderrunner@19 c5c08a46-e941-3541-a803-61b983f3127b

---
+++ MurderRunner/Assets/BigShockwave.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigShockwave : AttackBase {
+
+
+	
+	public override float Damage {
+		get;
+		set;
+	}
+	
+	public override float Range {
+		get;
+		set;
+	}
+	
+	public override float timeRemaining {
+		get;
+		set;
+	}
+	
+	private GameObject attackGameObject;
+	
+	public float attackDuration;
+	private Vector3 newDirection;
+	private Vector3 PointOfO; 
+	private float travelSpeed;
+
+
+
+	
+	public override void attack(Vector3 direction, Vector3 pointOfOrigin)
+	{
+		//		attackGameObject.SetActive (true);
+		timeRemaining = attackDuration;
+
+		attackGameObject.GetComponent<BoxCollider>().size = startingScale;
+
+
+		attackGameObject.renderer.material = (Resources.Load("BloodMaterial", typeof(Material)) as Material);
+		attackGameObject.collider.transform.position = pointOfOrigin;
+		newDirection = direction - pointOfOrigin;
+		//attackGameObject.collider.transform.rotation = Quaternion.FromToRotation (attackGameObject.collider.transform.forward, newDirection);
+		//attackGameObject.collider.transform.position = pointOfOrigin; 
+		PointOfO = pointOfOrigin; 
+		//Debug.Log ("New Direction X : " + newDirection.x + ", Y: " + newDirection.y + " , Z : " + newDirection.z);
+		//Debug.Log ("Point of Origin X : " + PointOfO.x + ", Y: " + PointOfO.y + " , Z : " + PointOfO.z);
+		
+		attackGameObject.collider.transform.rotation = Quaternion.LookRotation(newDirection) ;
+		attackGameObject.collider.transform.position += (Vector3.Normalize (newDirection) * (Range / 2));
+		//attackGameObject.collider.transform.Rotate (new Vector3 (0.0f, 1.0f, 0.0f), -90);
+		//attackGameObject.collider.transform.Translate (Vector3.Normalize (newDirection) * (Range / 2));
+		
+		
+		
+		attackGameObject.collider.enabled = true;
+		attackGameObject.GetComponent<MeshRenderer> ().material.SetColor ("_Color", Color.red);
+		
+		//attackGameObject.transform.position = pointOfOrigin;
+		
+		
+		
+		timeRemaining = attackDuration;
+		attackGameObject.name = "Weapon";
+
+
+		
+		
+	}
+
+
+
+
+
+	private Vector3 startingScale;
+	private float scaleRate ; 
+	// Use this for initialization
+	void Start () {
+		Damage = 5.0f;
+		Range = 4.5f;
+		attackDuration = 0.33f;
+		travelSpeed = 40.0f; 
+		scaleRate = 50.0f;
+		
+		attackGameObject = new GameObject ();
+		attackGameObject.AddComponent<MeshRenderer> ();
+
+		startingScale = new Vector3 (Range, 1, 1);
+
+
+		
+		attackGameObject.AddComponent<BoxCollider> ();
+		attackGameObject.GetComponent<BoxCollider>().size = startingScale;
+		Quaternion q = new Quaternion ();
+		attackGameObject.collider.transform.rotation = q;
+		
+		attackGameObject.collider.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine (PointOfO, PointOfO+newDirection);
+		Debug.DrawLine( PointOfO+newDirection, PointOfO+newDirection+new Vector3( 0, 4, 0 ) );
+		
+		if (timeRemaining > 0.0f) {
+			timeRemaining -= Time.deltaTime;
+			Vector3 direction = Vector3.Normalize(newDirection);
+			attackGameObject.collider.transform.position += direction*travelSpeed*Time.deltaTime;
+			attackGameObject.GetComponent<BoxCollider>().size = new Vector3( attackGameObject.GetComponent<BoxCollider>().size.x +Time.deltaTime*scaleRate, startingScale.y, startingScale.z);
+		} else {
+			attackGameObject.collider.enabled = false;
+			
+			//attackGameObject.SetActive(false);
+			
+		}
+
+	}
+}
---
+++ MurderRunner/Assets/Swing.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Swing : AttackBase {
+
+	
+	
+	public override float Damage {
+		get;
+		set;
+	}
+	
+	public override float Range {
+		get;
+		set;
+	}
+	
+	public override float timeRemaining {
+		get;
+		set;
+	}
+	
+	private GameObject attackGameObject;
+	
+	public float attackDuration;
+	private Vector3 newDirection;
+	private Vector3 PointOfO; 
+	private float travelSpeed;
+	
+	
+	
+	
+	public override void attack(Vector3 direction, Vector3 pointOfOrigin)
+	{
+		//		attackGameObject.SetActive (true);
+		timeRemaining = attackDuration;
+		
+		attackGameObject.GetComponent<BoxCollider>().size = startingScale;
+		
+		
+		attackGameObject.renderer.material = (Resources.Load("BloodMaterial", typeof(Material)) as Material);
+		attackGameObject.collider.transform.position = pointOfOrigin;
+		newDirection = direction - pointOfOrigin;
+		//attackGameObject.collider.transform.rotation = Quaternion.FromToRotation (attackGameObject.collider.transform.forward, newDirection);
+		//attackGameObject.collider.transform.position = pointOfOrigin; 
+		PointOfO = pointOfOrigin; 
+		//Debug.Log ("New Direction X : " + newDirection.x + ", Y: " + newDirection.y + " , Z : " + newDirection.z);
+		//Debug.Log ("Point of Origin X : " + PointOfO.x + ", Y: " + PointOfO.y + " , Z : " + PointOfO.z);
+		
+		attackGameObject.collider.transform.rotation = Quaternion.LookRotation(newDirection) ;
+		attackGameObject.collider.transform.position += (Vector3.Normalize (newDirection) * (Range / 2));
+		attackGameObject.collider.transform.Rotate (new Vector3 (0.0f, 1.0f, 0.0f), -90);
+		attackGameObject.collider.transform.Translate(new Vector3 (0.0f, 0.0f, 5.0f));
+
+		
+
+		//attackGameObject.collider.transform.Translate (Vector3.Normalize (newDirection) * (Range / 2));
+		
+		
+		
+		attackGameObject.collider.enabled = true;
+		attackGameObject.GetComponent<MeshRenderer> ().material.SetColor ("_Color", Color.red);
+		
+		//attackGameObject.transform.position = pointOfOrigin;
+		
+		
+		
+		timeRemaining = attackDuration;
+		
+		
+	}
+	
+	
+	
+	
+	
+	private Vector3 startingScale;
+	private float slashSpeed ; 
+	private float width = 1.5f; 
+	// Use this for initialization
+	void Start () {
+		Damage = 5.0f;
+		Range = 10.0f;
+		attackDuration = 0.08f;
+		travelSpeed = 40.0f; 
+		slashSpeed = 90.0f;
+		
+		attackGameObject = new GameObject ();
+		attackGameObject.AddComponent<MeshRenderer> ();
+		
+		startingScale = new Vector3 (Range, 1, width);
+		
+		
+		
+		attackGameObject.AddComponent<BoxCollider> ();
+		attackGameObject.GetComponent<BoxCollider>().size = startingScale;
+		Quaternion q = new Quaternion ();
+		attackGameObject.collider.transform.rotation = q;
+		
+		attackGameObject.collider.enabled = false;
+		attackGameObject.name = "Weapon";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawLine (PointOfO, PointOfO+newDirection);
+		Debug.DrawLine( PointOfO+newDirection, PointOfO+newDirection+new Vector3( 0, 4, 0 ) );
+		
+		if (timeRemaining > 0.0f) {
+			attackGameObject.collider.transform.Translate( new Vector3(0.0f,0.0f,-slashSpeed*Time.deltaTime));
+
+			timeRemaining -= Time.deltaTime;
+		} else {
+			attackGameObject.collider.enabled = false;
+			
+			//attackGameObject.SetActive(false);
+			
+		}
+		
+	}
+}
