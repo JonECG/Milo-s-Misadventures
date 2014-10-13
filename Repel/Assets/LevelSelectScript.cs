94d3bf8f386ae772223cdfcb469aae1be6405b09
Anson Wayman <Kanoyugoro@gmail.com>
2014-10-13 17:45:13 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/murderrunner@35 c5c08a46-e941-3541-a803-61b983f3127b

---
+++ Repel/Assets/LevelSelectScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelSelectScript : MonoBehaviour {
+
+
+	int buttonWidth = 100;
+	int buttonHeight = 50;
+	int numColumns = 4;
+	int numRows = 10;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		buttonWidth = Screen.width/numColumns;
+		buttonHeight = Screen.height/numRows;
+	}
+
+	bool customButton (int row, int column, string text)
+	{
+		return GUI.Button (new Rect (buttonWidth*column, buttonHeight*row, buttonWidth, buttonHeight), text);
+	}
+
+	void OnGUI() 
+	{
+		if (GUI.Button (new Rect (10, 10, buttonWidth/2, buttonHeight), "Back"))
+		{
+			//Go to the main menu
+		}
+
+		if (customButton (1,1,"Tutorial"))
+		{
+			
+		}
+
+		if (customButton (2, 1, "JumpNDash")) 
+		{
+
+		}
+
+		if (customButton (1,2,"Advanced"))
+		{
+			
+		}
+	}
+}
