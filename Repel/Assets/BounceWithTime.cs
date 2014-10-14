7e3758572247af4db165bbebf8f36b5c2cc3a18d
Jonathan Pearl <jonathantpearl@gmail.com>
2014-10-14 16:39:10 +0000

    Added bouncy questionmark
    git-svn-id: https://svn.neumont.edu:8443/svn/murderrunner@36 c5c08a46-e941-3541-a803-61b983f3127b

---
+++ Repel/Assets/BounceWithTime.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceWithTime : MonoBehaviour {
+
+	public float bounceTime = 2;
+	public float bounceHeight = 2;
+	
+	private float currentTime;
+	private Vector3 startOffset;
+	private Vector3 startScale;
+	
+	// Use this for initialization
+	void Start () {
+		currentTime = 0;
+		startOffset = this.transform.localPosition;
+		startScale = this.transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentTime += Time.deltaTime;
+		currentTime = currentTime % bounceTime;
+		
+		this.transform.localPosition = startOffset + new Vector3( 0, bounceHeight * Mathf.Max( 0, Mathf.Sin( Mathf.PI * currentTime/bounceTime ) ) );
+		this.transform.localScale = new Vector3( this.transform.localScale.x, startScale.y * ( -Mathf.Sin( Mathf.PI * currentTime/bounceTime )*0.5f + 1.5f ), startScale.z * ( Mathf.Sin( Mathf.PI * currentTime/bounceTime )*0.5f + 1 ) );
+	}
+}
--- Repel/Assets/EnergyController.cs
+++ Repel/Assets/EnergyController.cs
@@ -13,7 +13,11 @@ public class EnergyController : MonoBehaviour {
 	void Start () {
 		GameObject.Find( "LevelController" ).GetComponent<LevelController>().energies.Add( this.gameObject );
 		if( tutorial )
-			this.renderer.material.color = new Color( 0.5f, 1, 0 );
+		{
+			((GameObject)Instantiate( Resources.Load( "QuestionPref", typeof( GameObject ) ), this.transform.position, Quaternion.Euler( -90, 0, -90 ) )).transform.parent = this.transform;
+			//this.gameObject
+			//this.renderer.material.color = new Color( 0.5f, 1, 0 );
+		}
 	}
 	
 	// Update is called once per frame
