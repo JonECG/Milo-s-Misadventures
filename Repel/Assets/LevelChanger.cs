445a3922b73b50a2e04995f02cbd2dba39711bbe
Colter Cvach <ccvach@student.neumont.edu>
2014-10-20 17:21:58 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/murderrunner@45 c5c08a46-e941-3541-a803-61b983f3127b

---
+++ Repel/Assets/LevelChanger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChanger : MonoBehaviour {
+
+	public string toLoad;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			RaycastHit hit = new RaycastHit ();
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray, out hit, 200)) {
+				resolveCollision (hit);
+			}
+		}
+	}
+
+	private void resolveCollision(RaycastHit hit)
+	{
+		if (hit.collider==this.gameObject.collider) {
+			Application.LoadLevel(toLoad);
+		}
+		
+	}
+
+
+
+}
