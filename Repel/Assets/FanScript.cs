b8b192319d2cc8d91c3db7f67d27731d0e9259cd
Anson Wayman <Kanoyugoro@gmail.com>
2014-10-29 09:54:48 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/murderrunner@58 c5c08a46-e941-3541-a803-61b983f3127b

---
+++ Repel/Assets/FanScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FanScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameObject.Find( "LevelController" ).GetComponent<LevelController>().fans.Add( this.gameObject );
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
