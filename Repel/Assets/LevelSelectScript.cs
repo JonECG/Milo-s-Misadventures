e7adbb7c414cf37ef768f59d22920f20a8d2186d
Colter Cvach <ccvach@student.neumont.edu>
2014-11-11 17:23:42 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/murderrunner@78 c5c08a46-e941-3541-a803-61b983f3127b

--- Repel/Assets/LevelSelectScript.cs
+++ Repel/Assets/LevelSelectScript.cs
@@ -15,9 +15,13 @@ public class LevelSelectScript : MonoBehaviour {
 	Vector3 startPos; 
 	GameObject text; 
 	float totalDis=0.0f;
+
+	int currentButtonCount= 0 ; 
 	// Use this for initialization
 	void Start () 
 	{
+		yDisplacement = 0.0f;
+		//totalDis = -30;
 		text = GameObject.Find ("Title");
 		startPos = text.transform.position;
 	}
@@ -25,34 +29,59 @@ public class LevelSelectScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
+
+
+
 		buttonWidth = Screen.width/numColumns;
 		buttonHeight = Screen.height/numRows;
 
 		if (!mouseWasDown && Input.GetMouseButton (0)) {
 						prevY = Input.mousePosition.y;
-			mouseWasDown=true;
+						mouseWasDown=true;
 				} else if (Input.GetMouseButton (0)) {
-			yDisplacement = prevY-Input.mousePosition.y;
+
+			yDisplacement += ((prevY-Input.mousePosition.y));
+			prevY = Input.mousePosition.y;
 				} else {
 			mouseWasDown =false;
+			//yDisplacement=0.0f;
 				}
-		totalDis += yDisplacement/5;
-		Vector3 newPos = new Vector3 (startPos.x, text.gameObject.transform.position.y -(yDisplacement/Screen.height)/5, startPos.z);
-		text.gameObject.transform.position = newPos;
-		yDisplacement = 0.0f;
-
+		Debug.Log ("TotalDis: " +totalDis);
+		totalDis = yDisplacement;
+		//Vector3 newPos = new Vector3 (startPos.x, text.gameObject.transform.position.y -(yDisplacement/Screen.height)/2, startPos.z);
+		//text.gameObject.transform.position = newPos;
+		//yDisplacement = 0.0f;
+		//startPos = newPos;
 		text.gameObject.SetActive(false);
+
+		if (totalDis > Screen.height / 8.0f) {
+			yDisplacement-=totalDis-(Screen.height/8.0f);
+			totalDis = Screen.height /8.0f;
+		}
+		else if (totalDis + ((currentButtonCount*1.5f)*buttonHeight) < Screen.height*2.0f / 3.0f) {
+			yDisplacement-= (totalDis-((Screen.height*2.0f/3.0f)-(currentButtonCount*1.5f*buttonHeight)));
+			totalDis = (float)(Screen.height*2.0f /3.0f)-(currentButtonCount*1.5f*buttonHeight);
+		}
+		
+
+
+
+
 	}
 
 	bool checkButton(int num, string text)
 	{
+		if (num > currentButtonCount) {
+			currentButtonCount=num;
+				}
+
 		return customButton (1.5f * num, 1.5f, ""+num+") "+text);
 	}
 
 
 	bool customButton (float row, float column, string text)
 	{
-		return GUI.Button (new Rect (buttonWidth*column, buttonHeight*row + totalDis/5, buttonWidth, buttonHeight), text);
+		return GUI.Button (new Rect (buttonWidth*column, buttonHeight*row + totalDis, buttonWidth, buttonHeight), text);
 	}
 
 	void OnGUI() 
@@ -63,6 +92,18 @@ public class LevelSelectScript : MonoBehaviour {
 			//Go to the main menu
 		}
 
+
+		if (checkButton(1, "Tutorial: New Heights")) 
+		{
+			Application.LoadLevel( "UpTutorial" );
+		}
+
+		
+		if (checkButton (2,"Tutorial: Dashing along"))
+		{
+			Application.LoadLevel( "TalansLevel" );
+		}
+
 		if (checkButton(3,"Tutorial: Up and up"))
 		{
 			Application.LoadLevel( "JumpAndDownPractice_1" );
@@ -71,35 +112,27 @@ public class LevelSelectScript : MonoBehaviour {
 		{
 			Application.LoadLevel( "leftTutorial" );
 		}
-
-
+		if (checkButton (5, "Closed Spaces"))
+		{
+			Application.LoadLevel( "AJLevel" );
+		}
 		
-		if (checkButton (2,"Tutorial: Dashing along"))
-		{
-			Application.LoadLevel( "TalansLevel" );
+		if (checkButton (6, "FANS")) {
+			Application.LoadLevel ("AJLevel2");
+		}
+		
+		if (checkButton (7, "Combined Skills")) {
+			Application.LoadLevel ("AJLevel3");
 		}
 
-		if (checkButton(1, "Tutorial: New Heights")) 
-		{
-			Application.LoadLevel( "UpTutorial" );
-		}
+
+
 // 		if (checkButton (1, "Hold it!")) 
 // 		{
 // 			Application.LoadLevel( "testScene" );
 // 		}
 
-		if (checkButton (5, "Closed Spaces"))
-		{
-			Application.LoadLevel( "AJLevel" );
-		}
 
-		if (checkButton (6, "FANS")) {
-			Application.LoadLevel ("AJLevel2");
-		}
-
-		if (checkButton (7, "Combined Skills")) {
-			Application.LoadLevel ("AJLevel3");
-		}
 
 
 
