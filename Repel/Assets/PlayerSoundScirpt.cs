85aa0e5a75706636a9989918896e126ff901f8bd
Colter Cvach <ccvach@student.neumont.edu>
2014-11-17 21:59:17 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/murderrunner@101 c5c08a46-e941-3541-a803-61b983f3127b

---
+++ Repel/Assets/PlayerSoundScirpt.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSoundScirpt : MonoBehaviour {
+
+	private static PlayerSoundScirpt instance = null;
+	public static PlayerSoundScirpt Instance {
+		get { return instance; }
+	}
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+
+	public AudioClip[] enterLevel;
+	public AudioClip[] death;
+	public AudioClip[] jump;
+	public AudioClip[] superJump;
+	public AudioClip[] dash;
+	public AudioClip[] dive;
+	public AudioClip[] stop;
+
+
+	public void playEnterLevel()
+	{
+		int toPlay = (int)Random.Range (0.0f, enterLevel.Length-0.51f);
+		audio.clip = enterLevel [toPlay];
+		audio.Play ();
+	}
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
